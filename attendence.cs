998ae2c6d135d568e13910a3adbe7f6b204e078c
akhil2502 <akhilachilies>
2021-07-28 11:54:34 +0530

    [akhil] Add:wages and line

---
+++ attendence.cs
@@ -0,0 +1,23 @@
+﻿namespace practice1
+{
+    class Program
+    {using System;
+
+
+       public static void Main(string[] args)
+        {
+            Random random = new Random();
+            int  empCHeck = random.Next(0, 2);
+            Console.WriteLine("enter random number");
+
+            if (empCHeck == 0)
+                Console.WriteLine("employee present");
+            else
+                Console.WriteLine("employee absent");
+
+
+        }
+      
+        }
+
+}
