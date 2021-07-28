b3a7501a6335e95af4506a145ec8d02898a579f9
akhil2502 <akhilachilies>
2021-07-28 11:57:30 +0530

    [akhil] Add:salary

---
+++ salary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace salary
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int wage_per_hr = 30;
+            const int full_time = 8;
+
+
+            int total_salary = wage_per_hr * full_time;
+            Console.WriteLine("total_salary" + total_salary);
+        }        
+    }
+}
